c07879edb683845eca8475f989efe428226561d5
github username <github email address>
2020-09-12 16:39:57 -0500

    Updates

---
+++ precache-manifest.9eaed679a47318a3b669bb9486128962.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a6f4903e3eb93a3ef66dbfd858f060b",
+    "url": "/constellation-synth/index.html"
+  },
+  {
+    "revision": "cbe55174ff0409678ed8",
+    "url": "/constellation-synth/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "1585d35476d1b7533261",
+    "url": "/constellation-synth/static/css/main.64544bd7.chunk.css"
+  },
+  {
+    "revision": "cbe55174ff0409678ed8",
+    "url": "/constellation-synth/static/js/2.f58f921c.chunk.js"
+  },
+  {
+    "revision": "cb4293a028d0175415ddb31dc1aaaefb",
+    "url": "/constellation-synth/static/js/2.f58f921c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1585d35476d1b7533261",
+    "url": "/constellation-synth/static/js/main.52e8b0a4.chunk.js"
+  },
+  {
+    "revision": "e7ee271ec313ae0f9cd2",
+    "url": "/constellation-synth/static/js/runtime-main.43c9add3.js"
+  }
+]);
